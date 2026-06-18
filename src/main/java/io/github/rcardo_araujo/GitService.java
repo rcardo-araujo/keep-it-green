330418f77c6debdb19dd10b54160a1bad5d3d27a
Ricardo Araujo <ricardoasf@id.uff.br>
2026-06-18 20:10:43 -0300

    refactor: change .directory() ProcessBuilder argument to File type

--- src/main/java/io/github/rcardo_araujo/GitService.java
+++ src/main/java/io/github/rcardo_araujo/GitService.java
@@ -1,5 +1,6 @@
 package io.github.rcardo_araujo;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.Process;
 import java.lang.ProcessBuilder;
@@ -13,11 +14,11 @@ public class GitService {
                 "--allow-empty",
                 "-m",
                 (message != null) ? message : "ghost commit"
-            ).directory(directory);
+            ).directory(new File(directory));
 
             Process process = processBuilder.start();
 
-        } catch (IOException | InterruptedException exception) {
+        } catch (IOException exception) {
             exception.printStackTrace();
         }
     }
