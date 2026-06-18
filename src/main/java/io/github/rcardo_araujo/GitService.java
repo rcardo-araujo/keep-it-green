fe348bf5536fce4e887bb75bcded48e475a58f2d
Ricardo Araujo <ricardoasf@id.uff.br>
2026-06-18 20:44:33 -0300

    chore: move git log logic to GitService getLog() method

--- src/main/java/io/github/rcardo_araujo/GitService.java
+++ src/main/java/io/github/rcardo_araujo/GitService.java
@@ -1,11 +1,49 @@
 package io.github.rcardo_araujo;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.Process;
 import java.lang.ProcessBuilder;
+import java.util.ArrayList;
 
 public class GitService {
+    public ArrayList<String> getLog(String repository, String author) {
+        ArrayList<String> terminalLines = new ArrayList<String>();
+
+        String sinceDate = "2026-06-01";
+
+        try {    
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                "git", 
+                "log", 
+                "--author=" + author,
+                "--since=" + sinceDate,
+                "--pretty=format:%aI %s"
+            ).directory(new File(repository));
+
+            Process process = processBuilder.start();
+
+            try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream())
+            )) {
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    terminalLines.add(line);
+                }
+            }
+
+            int exitCode = process.waitFor();
+
+        } catch (IOException | InterruptedException exception) {
+            exception.printStackTrace();
+        }
+
+        return terminalLines;
+    }
+
     public void commit(String directory, String message, String date) {
         try {
             ProcessBuilder processBuilder = new ProcessBuilder(
--- src/main/java/io/github/rcardo_araujo/Main.java
+++ src/main/java/io/github/rcardo_araujo/Main.java
@@ -33,47 +33,14 @@ public class Main {
         GitService gitService = new GitService();
 
         for (String repo: appConfig.getReposToWatch()) {
-            try {    
-                String sinceDate = "2026-06-01";
+            ArrayList<String> logs = gitService.getLog(repo, appConfig.getAuthorEmail());
 
-                ProcessBuilder processBuilder = new ProcessBuilder(
-                    "git", 
-                    "log", 
-                    "--author=" + appConfig.getAuthorEmail(),
-                    "--since=" + sinceDate,
-                    "--pretty=format:%aI %s"
-                ).directory(new File(repo));
+            for (String log: logs) {
+                String[] commitArguments = log.split(" ", 2);
+                String commitDate = commitArguments[0];
+                String commitMessage = commitArguments[1];
 
-                Process process = processBuilder.start();
-
-                ArrayList<String> terminalLines = new ArrayList<String>();
-
-                try (BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(process.getInputStream())
-                )) {
-                    String line;
-
-                    while ((line = reader.readLine()) != null) {
-                        terminalLines.add(line);
-                    }
-                }
-
-                int exitCode = process.waitFor();
-
-                for (String line: terminalLines) {
-                    System.out.println(line);
-
-                    String[] commitArguments = line.split(" ", 2);
-                    String commitDate = commitArguments[0];
-                    String commitMessage = commitArguments[1];
-
-                    gitService.commit(appConfig.getDummyRepoPath(), commitMessage, commitDate);
-                }
-
-                System.out.println("\nProcess finished with code: " + exitCode);
-
-            } catch (IOException | InterruptedException exception) {
-                exception.printStackTrace();
+                gitService.commit(appConfig.getDummyRepoPath(), commitMessage, commitDate);
             }
         }
     }
