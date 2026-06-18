7a1b64f0678457a43c41699b8cbf5cc3ef8dae7f
Ricardo Araujo <ricardoasf@id.uff.br>
2026-06-18 19:39:13 -0300

    feat: add reposToWatch git log read

--- src/main/java/io/github/rcardo_araujo/Main.java
+++ src/main/java/io/github/rcardo_araujo/Main.java
@@ -3,22 +3,69 @@ package io.github.rcardo_araujo;
 import io.github.rcardo_araujo.AppConfig;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.InterruptedException;
+import java.lang.Process;
+import java.lang.ProcessBuilder;
+import java.util.ArrayList;
 import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) {
         final String configPath = "config.json";
 
-        com.google.gson.Gson gson = new Gson();
+        Gson gson = new Gson();
+        AppConfig appConfig = new AppConfig();
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(configPath));
 
-            AppConfig appConfig = gson.fromJson(reader, AppConfig.class);
+            appConfig = gson.fromJson(reader, AppConfig.class);
+            
         } catch(IOException exception) {
             exception.printStackTrace();
+        }
+
+        for (String repo: appConfig.getReposToWatch()) {
+            try {    
+                String sinceDate = "2026-06-01";
+
+                ProcessBuilder processBuilder = new ProcessBuilder(
+                    "git", 
+                    "log", 
+                    "--author=" + appConfig.getAuthorEmail(),
+                    "--since=" + sinceDate,
+                    "--oneline"
+                ).directory(new File(repo));
+
+                Process process = processBuilder.start();
+
+                ArrayList<String> terminalLines = new ArrayList<String>();
+
+                try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream())
+                )) {
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        terminalLines.add(line);
+                    }
+                }
+
+                int exitCode = process.waitFor();
+
+                for (String line: terminalLines) {
+                    System.out.println(line);
+                }
+
+                System.out.println("\nProcess finished with code: " + exitCode);
+
+            } catch (IOException | InterruptedException exception) {
+                exception.printStackTrace();
+            }
         }
     }
 }
