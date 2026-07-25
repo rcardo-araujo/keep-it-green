050252edb1e253aa5f8f93d2ff2b734e3acf5b53
Ricardo Araujo <ricardoasf@id.uff.br>
2026-07-24 23:59:11 -0300

    refactor: update electron-vite config file to ui/ and bridge/ folders

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,16 +1,28 @@
-import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "electron-vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  main: {},
-  preload: {},
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+    main: {},
+    preload: {
+        build: {
+            rollupOptions: {
+                input: resolve(__dirname, "src/bridge/index.ts")
+            }
+        }
     },
-    plugins: [react()]
-  }
-})
+    renderer: {
+        root: "src/ui",
+        build: {
+            rollupOptions: {
+                input: resolve(__dirname, "src/ui/index.ts")
+            }
+        },
+        resolve: {
+            alias: {
+                "@ui": resolve("src/ui/src")
+            }
+        },
+        plugins: [react()]
+    }
+});
