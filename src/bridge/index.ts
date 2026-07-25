0733af1722b88f94dd1954efaef751a244146d0b
Ricardo Araujo <ricardoasf@id.uff.br>
2026-07-25 16:58:21 -0300

    feat: add getSyncProfile and saveSyncProfile to bridge api

--- src/bridge/index.ts
+++ src/bridge/index.ts
@@ -1,22 +1,23 @@
-import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge, ipcRenderer } from "electron";
+import { electronAPI } from "@electron-toolkit/preload";
 
-// Custom APIs for renderer
-const api = {}
+const api = {
+    getSyncProfile: () => ipcRenderer.invoke("getSyncProfile"),
+    saveSyncProfile: (authorEmail: string, destinationRepoPath: string, sourceRepoPaths: string[]) => {
+        return ipcRenderer.invoke("saveSyncProfile", authorEmail, destinationRepoPath, sourceRepoPaths)
+    }
+};
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
 if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
-  } catch (error) {
-    console.error(error)
-  }
+    try {
+        contextBridge.exposeInMainWorld("electron", electronAPI);
+        contextBridge.exposeInMainWorld("api", api);
+    } catch (error) {
+        console.error(error);
+    }
 } else {
-  // @ts-ignore (define in dts)
-  window.electron = electronAPI
-  // @ts-ignore (define in dts)
-  window.api = api
+    // @ts-ignore (define in dts)
+    window.electron = electronAPI;
+    // @ts-ignore (define in dts)
+    window.api = api;
 }
